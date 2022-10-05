459f5540a5672b8b57db8a8898984aca8156a94e
Prosper Banda <probanda2018@gmail.com>
2022-10-04 22:58:59 -0500

    Updated my app with new features

--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.flashcardapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,28 @@ class MainActivity : AppCompatActivity() {
             flashcardQuestion.visibility = View.INVISIBLE
             flashcardAnswer.visibility = View.VISIBLE
         }
-        
+        flashcardAnswer.setOnClickListener {
+            flashcardAnswer.visibility = View.INVISIBLE
+            flashcardQuestion.visibility = View.VISIBLE
+        }
+
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            val data: Intent? = result.data
+            if (data != null){
+                val questionString = data.getStringExtra("QUESTION_KEY")
+                val answerString = data.getStringExtra("ANSWER_KEY")
+
+                flashcardQuestion.text = questionString
+                flashcardAnswer.text = answerString
+            }
+        }
+
+        val addQuestionbutton = findViewById<ImageView>(R.id.add_icon)
+        addQuestionbutton.setOnClickListener{
+            val intent = Intent(this, AddCardActivity::class.java)
+            resultLauncher.launch(intent)
+            resultLauncher.launch(intent)
+        }
     }
 }
